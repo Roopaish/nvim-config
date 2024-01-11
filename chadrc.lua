f789eb396ec937c8a182d3ed952a38df57a3513e
roopaish <rupesh39943@gmail.com>
2024-01-11 16:27:03 +0545

    added custom keymappings

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,11 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = { theme = "catppuccin" }
 M.plugins = "custom.plugins"
+vim.api.nvim_set_keymap("n", "<leader>d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<leader>d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
 return M
